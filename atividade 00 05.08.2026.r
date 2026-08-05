263a3f01373e863a164e350e8e4c9cf904ac1ee6
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-08-05 20:16:49 -0300

    adicionando print do olá mundo

--- atividade 00 05.08.2026.r
+++ atividade 00 05.08.2026.r
@@ -1,3 +1,5 @@
+print("Olá, Mundo!")
+
 numero <- 2
 # para entrada de dados abaixo
 # numero <- as.numeric(readline(prompt = "Digite um número para obtera tabuada:"))
@@ -7,4 +9,3 @@ cat("Tabuada do", numero, "\n")
 for (i in 1:10){
     resultado <- numero * i
     cat(numero, "x", i, "=", resultado, "\n")
-}
