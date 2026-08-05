e2e1b07dd0f39722dac71fafcd4da8c697a8f0a2
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-08-05 20:10:47 -0300

    exercícios da tabuada em r
    entrada de dados pelo compliador online não foi possível, coloquei o valor estático

---
+++ atividade 00 05.08.2026.r
@@ -0,0 +1,10 @@
+numero <- 2
+# para entrada de dados abaixo
+# numero <- as.numeric(readline(prompt = "Digite um número para obtera tabuada:"))
+
+cat("Tabuada do", numero, "\n")
+
+for (i in 1:10){
+    resultado <- numero * i
+    cat(numero, "x", i, "=", resultado, "\n")
+}
